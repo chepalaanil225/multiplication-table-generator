1b3a2863fe5a02c2101e0234445f7f26d0b42f06
Chepala Anil <chepalaanil225@gmail.com>
2025-09-05 15:32:07 +0530

    Update multiplicationtablegenerator.py

--- multiplicationtablegenerator.py
+++ multiplicationtablegenerator.py
@@ -2,4 +2,5 @@ num = int(input("Enter a number: "))
 
 for i in range(1, 13):
     print(f"{num} x {i} = {num * i}") 
+#range(1, 11) → generates numbers from 1 to 13 (remember, the second number 13 is not included).
     
